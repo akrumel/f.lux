11e9350268fcffea58d5e27568bfa1e2cd7a0bd5
Andrew Krumel <andykk@gmail.com>
2016-05-01 21:41:50 -0600

    Add createArrayPropertyType() and Shader to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 export { default as ArrayProperty } from "./ArrayProperty";
 export { default as ArrayShadow } from "./ArrayShadow";
+export { default as createArrayPropertyType } from "./createArrayPropertyType";
 export { default as IndexedProperty } from "./IndexedProperty";
 export { default as IndexedShadow } from "./IndexedShadow";
 export { default as KeyedProperty } from "./KeyedProperty";
@@ -9,6 +10,7 @@ export { default as MapShadow } from "./MapShadow";
 export { default as PrimitiveProperty } from "./PrimitiveProperty";
 export { default as PrimitiveShadow } from "./PrimitiveShadow";
 export { default as Property } from "./Property";
+export { default as Shader } from "./Shader";
 export { default as Shadow } from "./Shadow";
 export { default as StateTypes } from "./StateTypes";
 export { default as Store } from "./Store";
