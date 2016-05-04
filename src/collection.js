4221fc369801d3c8016be2bd726614033957382d
Andrew Krumel <andykk@gmail.com>
2016-05-04 11:59:13 -0600

    Fix collection exports

--- src/collection.js
+++ src/collection.js
@@ -1,7 +1,7 @@
 
-export { default as CollectionOptions } from "./collection/CollectionOptions";
+export * from "./collection/CollectionOptions";
+export * from "./collection/fetchOptions";
 export { default as CollectionProperty } from "./collection/CollectionProperty";
 export { default as CollectionShadow } from "./collection/CollectionShadow";
-export { default as fetchOptions } from "./collection/fetchOptions";
 export { default as RestEndpointProperty } from "./collection/RestEndpointProperty";
 export { default as RestQueryBuilder } from "./collection/RestQueryBuilder";
